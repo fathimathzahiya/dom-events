53470d105658edf6d7ffcf95ae5491c10fb3ca6f
fathimathzahiya <fathimathuzahiya1@gmail.com>
2026-04-23 15:16:25 +0530

    25 event  commit

---
+++ test25/s25.js
@@ -0,0 +1,7 @@
+const para = document.getElementById("para");
+para.addEventListener("mouseenter", () => {
+    para.style.color = "maroon";
+});
+para.addEventListener("mouseleave", () => {
+    para.style.color = "green";
+});
